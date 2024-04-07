f53c24c3514418533bb05663de93ac3382013592
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2024-04-07 17:32:02 +0530

    table sorting in second table ready for packing tab...adi 7april24

--- src/pages/Sales/Packing&Invoicing/Menu/Inspection/InspectionPages/Tabs/Tables/SecondTable.jsx
+++ src/pages/Sales/Packing&Invoicing/Menu/Inspection/InspectionPages/Tabs/Tables/SecondTable.jsx
@@ -1,20 +1,116 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 import { toast } from "react-toastify";
 
+import { FaArrowUp } from "react-icons/fa";
+
 export default function SecondTable(props) {
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
+
+  const sortedData = () => {
+    let dataCopy = [...props.invRegisterData];
+
+    if (sortConfig.key) {
+      dataCopy.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "asc" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "asc" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+
+    return dataCopy;
+  };
+
+  const requestSort = (key) => {
+    let direction = "asc";
+    if (sortConfig.key === key && sortConfig.direction === "asc") {
+      direction = "desc";
+    }
+    setSortConfig({ key, direction });
+  };
+
   return (
     <>
       <Table striped className="table-data border" style={{ border: "1px" }}>
         <thead className="tableHeaderBGColor">
           <tr>
             <th>Type</th>
-            <th>PN No</th>
-            <th>Status</th>
+            <th onClick={() => requestSort("DC_No")} className="cursor">
+              PN No
+              <FaArrowUp
+                className={
+                  sortConfig.key === "DC_No"
+                    ? sortConfig.direction === "desc"
+                      ? "rotateClass"
+                      : ""
+                    : "displayNoneClass"
+                }
+              />
+            </th>
+            <th onClick={() => requestSort("DCStatus")} className="cursor">
+              Status
+              <FaArrowUp
+                className={
+                  sortConfig.key === "DCStatus"
+                    ? sortConfig.direction === "desc"
+                      ? "rotateClass"
+                      : ""
+                    : "displayNoneClass"
+                }
+              />
+            </th>
           </tr>
         </thead>
         <tbody className="tablebody">
-          {props.invRegisterData?.map((val, key) => (
+          {sortedData().map((data, index) => (
+            <tr
+              onClick={(e) => {
+                // console.log("vallllllllll", data.DC_Inv_No);
+                props.setInvDetailsData([]);
+                props.setSelectedRegisterRow({});
+
+                if (
+                  props.selectedRegisterRow.DC_Inv_No === data.DC_Inv_No
+                  // props.invDetailsData.filter(
+                  //   (obj) => obj.DC_Inv_No === data.DC_Inv_No
+                  // )?.length > 0
+                ) {
+                  props.setInvDetailsData([]);
+                  props.setSelectedRegisterRow({});
+                } else {
+                  props.setSelectedRegisterRow(data);
+                  const newArray = props.allInvDetailsData.filter(
+                    (obj) => obj.DC_Inv_No === data.DC_Inv_No
+                  );
+
+                  if (newArray.length > 0) {
+                    props.setInvDetailsData(newArray);
+                  } else {
+                    toast.warning("No data found for the row");
+                  }
+                }
+              }}
+              className={
+                props.selectedRegisterRow.DC_Inv_No === data.DC_Inv_No
+                  ? // props.invDetailsData.filter(
+                    //   (obj) => obj.DC_Inv_No === data.DC_Inv_No
+                    // )?.length > 0
+                    "selectedRowClr"
+                  : ""
+              }
+              // props.selectedReadyForPackingRows.includes(data)
+            >
+              <td>{data.DC_InvType}</td>
+              <td>{data.DC_No}</td>
+              <td>{data.DCStatus}</td>
+            </tr>
+          ))}
+
+          {/* {props.invRegisterData?.map((val, key) => (
             <tr
               onClick={(e) => {
                 // console.log("vallllllllll", val.DC_Inv_No);
@@ -56,7 +152,7 @@ export default function SecondTable(props) {
               <td>{val.DC_No}</td>
               <td>{val.DCStatus}</td>
             </tr>
-          ))}
+          ))} */}
         </tbody>
       </Table>
     </>
